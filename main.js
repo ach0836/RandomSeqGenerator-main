b7b18340debf1edfa935d4045188cfda4e455410
Acetylcholin <jbs34_1211@js.hs.kr>
2025-01-08 19:25:43 +0900

    초기 프로젝트 파일 추가 및 기본 레이아웃 설정

---
+++ main.js
@@ -0,0 +1,236 @@
+'use strict';
+
+var myModal = new bootstrap.Modal(document.getElementById('modal'), {});
+myModal.toggle();
+var seqArray = [];
+var isOverlabed = false;
+
+function start() {
+	var num = option.txt.value;
+	var chk1 = option.chk1.checked;
+	var chk2 = option.chk2.checked;
+	if (num * 0 === 0 && num != "") {
+		if (3 <= num && num <= 10000 && num == Math.floor(num)) {
+			myModal.toggle();
+			for (var i = 0; i < num; i++) seqArray.push(i + 1);
+			for (var i = 0; i < num; i++) {
+				var rand = Math.floor(Math.random() * num);
+				var tmp = seqArray[i];
+				seqArray[i] = seqArray[rand];
+				seqArray[rand] = tmp;
+			}
+			if (chk1) isOverlabed = true;
+			if (chk2) {
+				var outter = document.getElementById('outter');
+				var inner = document.getElementById('field');
+				outter.className = 'd-flex flex-column justify-content-center vh-100';
+				inner.className = 'my-wmax my-hmax';
+			}
+		} else {
+			document.getElementById('lb1').style.display = 'none';
+			document.getElementById('lb2').style.display = 'block';
+		}
+	} else {
+		document.getElementById('lb1').style.display = 'block';
+		document.getElementById('lb2').style.display = 'none';
+	}
+}
+
+/* make canvas */
+
+const field = document.querySelector('#field');
+const myCanvas = document.getElementById('myCanvas');
+const ctx = myCanvas.getContext('2d');
+ctx.canvas.width = field.offsetWidth;
+ctx.canvas.height = field.offsetHeight;
+
+class cannonBall {
+	constructor() {
+		this.x = 0;
+		this.y = 0;
+		this.scale = 20;
+	}
+	draw() {
+		var nx = this.x + (myCanvas.width / 2);
+		var ny = this.y + (myCanvas.height / 5) + (600 / this.scale);
+		var radius = myCanvas.height / this.scale;
+		var gx = nx - radius;
+		var gy = ny - radius;
+		var gradient = ctx.createRadialGradient(gx, gy, radius / 2, nx, ny, radius * 2);
+		gradient.addColorStop(0, '#03ff00');
+		gradient.addColorStop(1, '#127909');
+		ctx.strokeStyle = 'rgba(0, 0, 0, 0)';
+		ctx.fillStyle = gradient;
+		ctx.beginPath();
+		ctx.arc(nx, ny, radius, 0, Math.PI * 2);
+		ctx.fill();
+		ctx.stroke();
+	}
+}
+
+class ballNum {
+	constructor() {
+		this.x = 0;
+		this.y = 0;
+		this.scale = 20;
+		this.num = 'none';
+	}
+	draw() {
+		var sz = String(myCanvas.height / this.scale / 1.5);
+		ctx.font = sz + "px Do Hyeon";
+		var nx = this.x + (myCanvas.width / 2) - (ctx.measureText(String(this.num)).width / 2);
+		var ny = this.y + (myCanvas.height / 5) + (700 / this.scale);
+		ctx.fillStyle = 'yellow';
+		ctx.fillText(this.num, nx, ny);
+	}
+}
+/*
+class cloud{
+	constructor(){
+		this.x = 0;
+		this.y = 0;
+		this.speed = 1;
+	}
+	draw(){
+		ctx.fillStyle = 'rgba(255, 255, 255, 0.5)';
+		ctx.beginPath();
+		ctx.arc(x, y, 100, 0, Math.PI * 2);
+		ctx.fill();
+		ctx.stroke();
+	}
+}
+*/
+var ball = new cannonBall();
+var number = new ballNum();
+//var _cloud = new cloud;
+
+/* Animation Control Variables */
+let isLooping = false;
+let lastTimestamp = null;
+
+/* launch */
+
+var cnt = 0;
+var numLog = document.getElementById('log');
+
+// 특정 숫자를 발사하는 함수
+function launchNumber(specificNum) {
+	if (isOverlabed) {
+		ball.scale = 20;
+		number.scale = 20;
+		number.num = specificNum;
+	} else {
+		if (cnt < seqArray.length) {
+			ball.scale = 20;
+			number.scale = 20;
+			number.num = specificNum;
+			cnt++; // 시퀀스 모드에서는 카운트를 증가시킵니다.
+		} else {
+			numLog.innerHTML += '<div class="log-inner font-dh">모든 공을 발사하였습니다.</div>';
+			numLog.scrollTop = numLog.scrollHeight;
+			return; // 더 이상 발사할 수 없습니다.
+		}
+	}
+
+	if (isOverlabed) {
+		numLog.innerHTML += `<div class="log-inner d-flex justify-content-between font-dh"><p>[Overlap] : </p><p>${number.num}번</p></div>`;
+	} else {
+		numLog.innerHTML += `<div class="log-inner d-flex justify-content-between font-dh"><p>[${cnt}] : </p><p>${number.num}번</p></div>`;
+	}
+	numLog.scrollTop = numLog.scrollHeight;
+
+	if (!isLooping) {
+		isLooping = true;
+		lastTimestamp = null;
+		requestAnimationFrame(loop);
+	}
+}
+
+// 기존 launch 함수는 시퀀스에 따라 발사합니다.
+function launch() {
+	if (isOverlabed) {
+		var randNum = seqArray[Math.floor(Math.random() * seqArray.length)];
+		launchNumber(randNum);
+	} else {
+		if (cnt < seqArray.length) {
+			var seqNum = seqArray[cnt];
+			launchNumber(seqNum);
+		} else {
+			numLog.innerHTML += '<div class="log-inner font-dh">모든 공을 발사하였습니다.</div>';
+			numLog.scrollTop = numLog.scrollHeight;
+		}
+	}
+}
+
+/* Key to Number Mapping */
+
+// 1-9: 키 '1' ~ '9'
+// 10-21: 키 'A' ~ 'L'
+const keyToNumberMap = {
+	'1': 1,
+	'2': 2,
+	'3': 3,
+	'4': 4,
+	'5': 5,
+	'6': 6,
+	'7': 7,
+	'8': 8,
+	'9': 9,
+	'q': 10,
+	'w': 11,
+	'e': 12,
+	'r': 13,
+	't': 14,
+	'y': 15,
+	'u': 16,
+	'i': 17,
+	'o': 18,
+	'p': 19,
+	'[': 20,
+	']': 21
+};
+
+// 키보드 이벤트 리스너 추가
+document.addEventListener('keydown', function (event) {
+	var key = event.key.toLowerCase(); // 대소문자 구분 없이 처리
+	if (keyToNumberMap.hasOwnProperty(key)) {
+		var numberToLaunch = keyToNumberMap[key];
+		launchNumber(numberToLaunch);
+	}
+});
+
+/* Animation Loop */
+
+function loop(timestamp) {
+	if (!lastTimestamp) lastTimestamp = timestamp;
+	const delta = timestamp - lastTimestamp;
+	lastTimestamp = timestamp;
+
+	ctx.canvas.width = field.offsetWidth;
+	ctx.canvas.height = field.offsetHeight;
+	ctx.clearRect(0, 0, myCanvas.width, myCanvas.height);
+
+	//	_cloud.draw();
+	ball.draw();
+	number.draw();
+
+	const scaleDecrementPerSecond = 25; // 스케일 감소 속도 (scale 단위/초)
+	const scaleDecrement = scaleDecrementPerSecond * (delta / 1000); // 프레임 간 시간 차이에 따른 감소량
+
+	if (ball.scale > 5) {
+		ball.scale -= scaleDecrement;
+		number.scale -= scaleDecrement;
+	} else {
+		// 애니메이션 종료
+		isLooping = false;
+		return;
+	}
+
+	requestAnimationFrame(loop);
+}
+
+/* ect */
+
+function reload() {
+	window.location.reload();
+}
