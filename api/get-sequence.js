0b1cea90f42e838d9123075a82be2de2ad99dd64
Acetylcholin <jbs34_1211@js.hs.kr>
2025-01-08 20:39:50 +0900

    CommonJS로 xata 모듈 가져오기 방식 변경

--- api/get-sequence.js
+++ api/get-sequence.js
@@ -1,4 +1,5 @@
-import { xata } from '../../xata';
+const { xata } = require('../../xata');
+
 
 export default async function handler(req, res) {
     if (req.method === 'GET') {
--- xata.js
+++ xata.js
@@ -1,6 +1,8 @@
-import { buildClient } from '@xata.io/client';
+const { buildClient } = require('@xata.io/client');
 
-export const xata = buildClient({
+const xata = buildClient({
     databaseURL: 'https://AN-CHAE-HEON-s-workspace-m2rvm3.us-east-1.xata.sh/db/canon:main',
     apiKey: process.env.XATA_API_KEY,
 });
+
+module.exports = { xata };
