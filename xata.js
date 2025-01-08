57ff5e5232cefb2bbae05675355d56ee207b0b7a
Acetylcholin <jbs34_1211@js.hs.kr>
2025-01-09 07:38:51 +0900

    데이터베이스 URL을 'random_sequences:main'으로 변경하고 Xata 클라이언트 정보를 로그에 출력하도록 수정

--- xata.js
+++ xata.js
@@ -1,8 +1,11 @@
 const { buildClient } = require('@xata.io/client');
 
 const xata = buildClient({
-    databaseURL: 'https://AN-CHAE-HEON-s-workspace-m2rvm3.us-east-1.xata.sh/db/canon:main',
+    databaseURL: 'https://AN-CHAE-HEON-s-workspace-m2rvm3.us-east-1.xata.sh/db/random_sequences:main',
     apiKey: process.env.XATA_API_KEY,
 });
+
+console.log('Xata Client:', xata);
+console.log('Xata DB:', xata.db);
 
 module.exports = { xata };
